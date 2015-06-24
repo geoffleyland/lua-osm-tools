6691e8ae9eeda39539fd843f119b3b8ec836a356
Geoff Leyland <geoff.leyland@incremental.co.nz>
2015-06-24 13:02:07 +1200

    So OSM ids are *nearly* but not actually unique across element types.  Why on earth not?  Since we're stuck with it, fake a unique id (4*osm_id + type)

--- lua/osmdb.lua
+++ lua/osmdb.lua
@@ -1,6 +1,24 @@
 local patengi = require"patengi"
 
 ------------------------------------------------------------------------------
+-- IDs aren't unique across nodes, types and ways.
+-- It's tempting to wonder why on earth they did that, but let's not dwell on
+-- it.
+-- Let's just create a unique ID that's 4*osm_id + type_code.
+-- We'll call in an "eid" for "element id".  My first idea "uid" (for
+-- "unique_id") is already taken by the user_id field
+
+local type_code =
+{
+  node          = 0,
+  way           = 1,
+  relation      = 2,
+}
+
+local NODE_CODE = type_code.node
+
+
+------------------------------------------------------------------------------
 
 local osmdb = {}
 osmdb.__index = osmdb
@@ -11,80 +29,80 @@ local function build_queries(db)
   {
     insert_element = db:prepare[[
       INSERT INTO elements
-        (id, type)
+        (eid, type)
       VALUES
-        (:id, :type);
+        (:eid, :type);
       ]],
     insert_info = db:prepare[[
       INSERT INTO info
-        (id, version, changeset, user, uid, timestamp)
+        (eid, version, changeset, user, uid, timestamp)
       VALUES
-        (:id, :version, :changeset, :user, :uid, :timestamp);
+        (:eid, :version, :changeset, :user, :uid, :timestamp);
       ]],
     insert_location = db:prepare[[
       INSERT INTO locations
-        (id, latitude, longitude)
+        (eid, latitude, longitude)
       VALUES
-        (:id, :latitude, :longitude);
+        (:eid, :latitude, :longitude);
       ]],
     insert_node = db:prepare[[
       INSERT INTO nodes
-        (parent_id, ord, node_id)
+        (parent_eid, ord, node_eid)
       VALUES
-        (:parent_id, :ord, :node_id);
+        (:parent_eid, :ord, :node_eid);
       ]],
     insert_member = db:prepare[[
       INSERT INTO members
-        (parent_id, ord, role, member_id)
+        (parent_eid, ord, role, member_eid)
       VALUES
-        (:parent_id, :ord, :role, :member_id);
+        (:parent_eid, :ord, :role, :member_eid);
       ]],
     insert_tag = db:prepare[[
       INSERT INTO tags
-        (parent_id, key, value)
+        (parent_eid, key, value)
       VALUES
-        (:parent_id, :key, :value);
+        (:parent_eid, :key, :value);
       ]],
 
     select_all_elements = db:prepare[[
-      SELECT id, type
+      SELECT eid, type
       FROM elements;
       ]],
     select_some_elements = db:prepare[[
-      SELECT id, type
+      SELECT eid, type
       FROM elements
       WHERE type = :1;
       ]],
     select_element = db:prepare[[
-      SELECT id, type
+      SELECT eid, type
       FROM elements
-      WHERE id = :id;
+      WHERE eid = :eid;
       ]],
     select_info = db:prepare[[
       SELECT version, changeset, user, uid, timestamp
       FROM info
-      WHERE id = :id;
+      WHERE eid = :eid;
       ]],
     select_location = db:prepare[[
       SELECT latitude, longitude
       FROM locations
-      WHERE id = :id;
+      WHERE eid = :eid;
       ]],
     select_tags = db:prepare[[
       SELECT key, value
       FROM tags
-      WHERE parent_id = :parent_id;
+      WHERE parent_eid = :parent_eid;
       ]],
     select_nodes = db:prepare[[
-      SELECT node_id
+      SELECT node_eid
       FROM nodes
-      WHERE parent_id = :parent_id
+      WHERE parent_eid = :parent_eid
       ORDER BY ord;
       ]],
     select_members = db:prepare[[
-      SELECT role, member_id
+      SELECT role, type, member_eid
       FROM members
-      WHERE parent_id = :parent_id
+      WHERE parent_eid = :parent_eid
       ORDER BY ord;
       ]],
   }
@@ -111,13 +129,13 @@ function osmdb.create(filename)
   db:exec[[
   CREATE TABLE elements
   (
-    id INTEGER PRIMARY KEY,
+    eid INTEGER PRIMARY KEY,
     type VARCHAR
   );]]
   db:exec[[
   CREATE TABLE info
   (
-    id INTEGER PRIMARY KEY,
+    eid INTEGER PRIMARY KEY,
     version INTEGER,
     changeset INTEGER,
     user VARCHAR,
@@ -127,31 +145,32 @@ function osmdb.create(filename)
   db:exec[[
   CREATE TABLE tags
   (
-    parent_id INTEGER,
+    parent_eid INTEGER,
     key VARCHAR,
     value VARCHAR
   );]]
   db:exec[[
   CREATE TABLE locations
   (
-    id INTEGER PRIMARY KEY,
+    eid INTEGER PRIMARY KEY,
     latitude REAL,
     longitude REAL
   );]]
   db:exec[[
   CREATE TABLE nodes
   (
-    parent_id INTEGER,
+    parent_eid INTEGER,
     ord INTEGER,
-    node_id INTEGER
+    node_eid INTEGER
   );]]
   db:exec[[
   CREATE TABLE members
   (
-    parent_id INTEGER,
+    parent_eid INTEGER,
     ord INTEGER,
     role VARCHAR,
-    member_id INTEGER
+    type VARCHAR,
+    member_eid INTEGER
   );]]
 
   return osmdb:new(db)
@@ -173,32 +192,27 @@ end
 
 function osmdb:commit()
   self.db:exec("COMMIT;")
-  self.db:exec("CREATE INDEX ni ON nodes (parent_id, ord);")
-  self.db:exec("CREATE INDEX mi ON members (parent_id, ord);")
-  self.db:exec("CREATE INDEX ti ON tags (parent_id);")
+  self.db:exec("CREATE INDEX ni ON nodes (parent_eid, ord);")
+  self.db:exec("CREATE INDEX mi ON members (parent_eid, ord);")
+  self.db:exec("CREATE INDEX ti ON tags (parent_eid);")
   self.db:exec("CREATE INDEX tki ON tags (key);")
   self.db:exec("VACUUM;")
   self.db:exec("ANALYZE;")
 end
 
 
-function osmdb:insert_tags(o)
+function osmdb:_insert_element(o, type)
   local values = self.values
-  values.parent_id = tonumber(o.id)
+  values.eid = tonumber(o.id) * 4 + type_code[type]
+  values.type = type
+  self.Q.insert_element:exec(values)
+
+  values.parent_eid = values.eid
   for k, v in pairs(o.tags) do
     values.key = k
     values.value = v
     self.Q.insert_tag:exec(values)
   end
-end
-
-
-function osmdb:_insert_element(o, type)
-  local values = self.values
-  values.id = tonumber(o.id)
-  values.type = type
-  self.Q.insert_element:exec(values)
-  self:insert_tags(o)
 
   if o.version or o.changeset or o.user or o.uid or o.timestamp then
     values.version = tonumber(o.version)
@@ -208,6 +222,8 @@ function osmdb:_insert_element(o, type)
     values.timestamp = o.timestamp and os.date("%Y-%m-%d %H:%M:%S", o.timestamp)
     self.Q.insert_info:exec(values)
   end
+
+  return values.eid
 end
 
 
@@ -222,27 +238,26 @@ end
 
 
 function osmdb:insert_way(o)
-  self:_insert_element(o, "way")
-
   local values = self.values
-  values.parent_id = tonumber(o.id)
+  values.parent_eid = self:_insert_element(o, "way")
+
   for i, n in ipairs(o.node_refs) do
     values.ord = i
-    values.node_id = tonumber(n)
+    values.node_eid = tonumber(n) * 4 + NODE_CODE
     self.Q.insert_node:exec(values)
   end
 end
 
 
 function osmdb:insert_relation(o)
-  self:_insert_element(o, "relation")
-
   local values = self.values
-  values.parent_id = tonumber(o.id)
+  values.parent_eid = self:_insert_element(o, "relation")
+
   for i, n in ipairs(o.member_refs) do
     values.ord = i
     values.role = n.role
-    values.member_id = tonumber(n.id)
+    values.type = n.type
+    values.member_id = tonumber(n.eid) * 4 + type_code[n.type]
     self.Q.insert_member:exec(values)
   end
 end
@@ -269,8 +284,11 @@ end
 
 function osmdb:read_element(element, with_children)
   with_children = with_children == "with_children" or with_children == true
+
+  element.id = math.floor(element.eid / 4)
+
   local version, changeset, user, uid, timestamp =
-    self.Q.select_info:uexec(element.id)
+    self.Q.select_info:uexec(element.eid)
   if version then
     element.version = version
     element.changeset = changeset
@@ -280,41 +298,41 @@ function osmdb:read_element(element, with_children)
   end
 
   element.tags = {}
-  for key, value in self.Q.select_tags:urows(element.id) do
+  for key, value in self.Q.select_tags:urows(element.eid) do
     element.tags[key] = value
   end
 
   if element.type == "node" then
     element.latitude, element.longitude =
-      self.Q.select_location:uexec(element.id)
+      self.Q.select_location:uexec(element.eid)
   elseif element.type == "way" then
     if with_children then
       element.nodes = {}
-      for node_id in self.Q.select_nodes:urows(element.id) do
-        local n = self.Q.select_element:nexec(node_id, "element")
+      for node_eid in self.Q.select_nodes:urows(element.eid) do
+        local n = self.Q.select_element:nexec(node_eid, "element")
         element.nodes[#element.nodes+1] = self:read_element(n)
       end
     else
       element.node_refs = {}
-      for node_id in self.Q.select_nodes:urows(element.id) do
-        element.node_refs[#element.node_refs+1] = node_id
+      for node_eid in self.Q.select_nodes:urows(element.eid) do
+        element.node_refs[#element.node_refs+1] = math.floor(node_eid / 4)
       end
     end
   elseif element.type == "relation" then
     if with_children then
       element.members = {}
-      for role, member_id in self.Q.select_members:urows(element.id) do
-        local m = self.Q.select_element:nexec(member_id)
+      for role, type, member_eid in self.Q.select_members:urows(element.eid) do
+        local m = self.Q.select_element:nexec(member_eid)
         if m then
           element.members[#element.members+1] =
-            { role = role, member = self:read_element(m) }
+            { role = role, type = type, member = self:read_element(m) }
         end
       end
     else
       element.member_refs = {}
-      for role, member_id in self.Q.select_members:urows(element.id) do
+      for role, type, member_eid in self.Q.select_members:urows(element.eid) do
         element.member_refs[#element.member_refs+1] =
-            { role = role, id = member_id }
+          { role = role, type = type, id = math.floor(member_eid / 4), eid = member_eid }
       end
     end
   end
